067c7c22632a85569ec49847f7c0b1fab0fbb06b
David <david.guajardo.o@gmail.com>
2018-01-14 23:35:51 -0300

    Primer Commit

---
+++ src/app/components/body.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    template: `
+        
+    `,
+    templateUrl: 'body.component.html'
+  })
+  export class BodyComponent {
+    
+  }
+  
